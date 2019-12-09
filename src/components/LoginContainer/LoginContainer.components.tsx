d57d641725be48850481f817618ef6944bc286b7
elnino07 <55378895+elnino07@users.noreply.github.com>
2019-12-09 11:22:19 +0100

    Update LoginContainer.components.tsx

--- src/components/LoginContainer/LoginContainer.components.tsx
+++ src/components/LoginContainer/LoginContainer.components.tsx
@@ -2,26 +2,26 @@ import React, { Component } from 'react'
 import "./LoginContainer.components.css"
 import HttpService, { HTTPMETHOD } from '../../services/http.services';
 
-export default class LoginContainer extends Component<{}, { email: string, password: string, credentials: any}> {
+export default class LoginContainer extends Component<{}, { email: string, password: string}> {
 
     constructor(props: any) {
         super(props);
 
         this.state = {
             email: '',
-            password: '',
-            credentials: {email: '', password: ''}
+            password: ''
         }
 
         this.sendLoginData = this.sendLoginData.bind(this);
     }
 
     sendLoginData() {
-        console.log(this.state.credentials)
-        HttpService.request(HTTPMETHOD.POST, '/auth/login', this.state.credentials)
+        let credentials = {email: this.state.email, password: this.state.password};
+        console.log(credentials)
+        HttpService.request(HTTPMETHOD.POST, '/auth/login', credentials)
             .then(res => {
                 console.log(res)
-                console.log(res.data)
+                // localStorage.setItem("jws", res.data.jwt) <--- not working at the moment (it should save the JWT token in the browsers local storage)
             })
             .catch(err => {
                 console.log(err)
@@ -29,20 +29,20 @@ export default class LoginContainer extends Component<{}, { email: string, passw
     }
 
     handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({credentials: {email: e.target.value}});
+        this.setState({email: e.target.value});
     } 
 
     handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({credentials: {password: e.target.value}})
+        this.setState({password: e.target.value})
     }
 
     render() {
         return (
-            <form>
-                <input type="text" name="email" placeholder="E-mail" value={this.state.credentials.email} onChange={this.handleEmailChange}></input>
-                <input type="password" name="password" placeholder="Password" value={this.state.credentials.password} onChange={this.handlePasswordChange}></input>
+            <div>
+                <input type="email" name="email" placeholder="E-mail" value={this.state.email} onChange={this.handleEmailChange}></input>
+                <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}></input>
                 <button onClick={this.sendLoginData}>send login data</button>
-            </form>
+            </div>
         )
     }
 }
