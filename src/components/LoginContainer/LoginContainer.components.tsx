562dcc76fc7870cf6bf410e3e02dd013865f74ee
elnino07 <55378895+elnino07@users.noreply.github.com>
2019-12-12 11:43:31 +0100

    updated login
    implemented also fail alert
    and added localStorage item: id

--- src/components/LoginContainer/LoginContainer.components.tsx
+++ src/components/LoginContainer/LoginContainer.components.tsx
@@ -1,26 +1,21 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import "./LoginContainer.components.css"
 import HttpService, { HTTPMETHOD } from '../../services/http.services';
 import { Redirect } from "react-router-dom"
 import SweetAlert from 'react-bootstrap-sweetalert'
 import {
     Formik,
-    Field,
     Form,
     useField,
-    FieldAttributes,
-    FieldArray
+    FieldAttributes
 } from "formik";
 import {
     TextField,
-    Button,
-    Checkbox,
-    Radio,
-    FormControlLabel,
-    Select,
-    MenuItem
+    Button
 } from "@material-ui/core";
 import * as yup from 'yup';
+import { Http2ServerResponse } from 'http2';
 
 const Mytextfield: React.FC<FieldAttributes<{}>> = ({
     placeholder,
@@ -57,40 +52,49 @@ export default class LoginContainer extends Component<{}, { email: string, passw
 
     validationSchema = yup.object({
         email: yup
-        .string()
-        .required()
-        .email(),
+            .string()
+            .required()
+            .email(),
         password: yup
-        .string()
-        .required()
+            .string()
+            .required()
     });
 
     Sign: React.FC = () => {
         return (
             <div>
                 <Formik
-                validateOnChange={true}
-                initialValues={{
-                    email: '',
-                    password: ''
-                }}
-                validationSchema={this.validationSchema}
-                onSubmit={(data, { setSubmitting }) => {
-                    setSubmitting(true);
-                    console.log('submit', data)
-                    setSubmitting(false);
-                }}
+                    validateOnChange={true}
+                    initialValues={{
+                        email: '',
+                        password: ''
+                    }}
+                    validationSchema={this.validationSchema}
+                    onSubmit={(data, { setSubmitting }) => {
+                        setSubmitting(true);
+                        console.log('submit', data)
+                        setSubmitting(false);
+                        this.setState({email: data.email, password: data.password})
+                        this.sendLoginData()
+                    }}
                 >
-                    {({isSubmitting}) => (
+                    {({ isSubmitting }) => (
                         <Form>
                             <Mytextfield placeholder='email' name="email" />
                             <div>
                                 <Mytextfield type="password" placeholder="password" name="password" />
                             </div>
                             <div>
-                                <Button disabled={isSubmitting} type="submit">
+                                {this.state.showSuccessAlert &&
+                                    <SweetAlert success title="Success!" onConfirm={this.onConfirm} timeout={3000}>
+                                        You are loggedin!
+                        </SweetAlert>
+                                }
+                            </div>
+                            <div>
+                                {this.state.redirect ? <Redirect to="/" /> : <Button disabled={isSubmitting} type="submit">
                                     submit
-                                </Button>
+                                </Button>}
                             </div>
                         </Form>
                     )}
@@ -103,8 +107,21 @@ export default class LoginContainer extends Component<{}, { email: string, passw
         this.setState({ redirect: !this.state.redirect })
     }
 
-    showAlert() {
+    showSuccessAlert() {
         this.setState({ showSuccessAlert: !this.state.showSuccessAlert })
+    }
+
+    onFailConfirm: React.FC = () => {
+        return(
+            <div>
+                {this.setState({ showFailAlert: !this.state.showFailAlert })}
+            <Redirect to="/login" />
+            </div>
+        )
+    }
+
+    showFailAlert() {
+        this.setState({ showFailAlert: !this.state.showFailAlert })
     }
 
     sendLoginData() {
@@ -112,19 +129,13 @@ export default class LoginContainer extends Component<{}, { email: string, passw
         HttpService.request(HTTPMETHOD.POST, '/auth/login', credentials)
             .then(res => {
                 localStorage.setItem("jws", res.data.jws)
-                this.showAlert()
+                localStorage.setItem("user", res.data.id)
+                this.showSuccessAlert()
             })
             .catch(err => {
+                this.showFailAlert()
                 console.log(err)
             })
-    }
-
-    handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ email: e.target.value });
-    }
-
-    handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ password: e.target.value })
     }
 
     render() {
@@ -132,18 +143,16 @@ export default class LoginContainer extends Component<{}, { email: string, passw
             <div className="login-components">
                 <this.Sign />
                 <div>
-                    <input className="log-input-email" type="email" name="email" placeholder="E-mail" value={this.state.email} onChange={this.handleEmailChange}></input>
-                    <input className="log-input-pw" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}></input>
-                </div>
-                <div>
-                    {this.state.redirect ?
-                        <Redirect to="/" /> : <button className="log-btn" onClick={this.sendLoginData}>send login data</button>
-                    }
-                </div>
-                <div>
                     {this.state.showSuccessAlert &&
                         <SweetAlert success title="Success!" onConfirm={this.onConfirm} timeout={3000}>
                             You are loggedin!
+                        </SweetAlert>
+                    }
+                </div>
+                <div>
+                    {this.state.showFailAlert &&
+                        <SweetAlert title="Login failed!" onConfirm={this.onFailConfirm}>
+                            Please check your e-mail and password!
                         </SweetAlert>
                     }
                 </div>
