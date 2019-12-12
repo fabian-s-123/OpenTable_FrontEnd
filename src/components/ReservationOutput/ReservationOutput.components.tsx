3aac3ec319c8b334a31717fc4397864fde8497e1
elnino07 <55378895+elnino07@users.noreply.github.com>
2019-12-12 22:14:22 +0100

    couple of changes
    implemented ReservationOutput
    added loading spinner into ReservationInput

--- src/components/ReservationOutput/ReservationOutput.components.tsx
+++ src/components/ReservationOutput/ReservationOutput.components.tsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import "./ReservationOutput.components.css"
+import { Divider } from '@material-ui/core'
 
-export default class ReservationOutput extends Component {
+export default class ReservationOutput extends Component<{ output: boolean }, {}> {
+
+    constructor(props: any) {
+        super(props)
+
+    }
+
     render() {
         return (
             <div>
-                
+                {this.props.output ? <div>Reservation successfull!</div> : <div>Reservation failed!</div>}
             </div>
         )
     }
