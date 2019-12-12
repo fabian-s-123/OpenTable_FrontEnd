618acb9ecc909531b3e2bd36e1e938689356ffa8
fabian-s-123 <55379268+fabian-s-123@users.noreply.github.com>
2019-12-12 11:48:30 +0100

    partial implementation

--- src/pages/Reservation/Reservation.page.tsx
+++ src/pages/Reservation/Reservation.page.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import "./Reservation.page.css"
 import RestaurantInfos from '../../components/RestaurantInfos/RestaurantInfos.components'
 import RestaurantImage from '../../components/RestaurantImage/RestaurantImage.components'
+import ReservationInput from '../../components/ReservationInput/ReservationInput.components'
 
 export default class Reservation extends Component<{location: any},{}> {
 
@@ -10,13 +11,19 @@ export default class Reservation extends Component<{location: any},{}> {
 
     render() {
         const img = this.props.location.state.data.images;
+        const rest = this.props.location.state.data;
         console.log(img)
         return (
             <div>
                 <div className="image-div">
-                    <RestaurantImage image={img} /></div>
+                    <RestaurantImage image={img} />
+                </div>
                 <div>
-                    <RestaurantInfos />Here you can make your Reservation!</div>
+                    <RestaurantInfos />Here you can make your Reservation!
+                </div>
+                <div className="reservation-form">
+                    <ReservationInput restaurant={rest}/>
+                </div>
             </div>
         )
     }
