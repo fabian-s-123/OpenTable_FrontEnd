dc8f033174ba23d5a1de62bf5446e7fd96df05ca
elnino07 <55378895+elnino07@users.noreply.github.com>
2019-12-13 12:24:37 +0100

    added structure

---
+++ src/components/Button/Button.components.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Button.components.css'
+
+export default class Button extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/InputField/InputField.components.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './InputField.components.css'
+
+export default class InputField extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
