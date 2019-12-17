fcf0ee509e255b2c57932081cca772ac8790661e
fabian-s-123 <55379268+fabian-s-123@users.noreply.github.com>
2019-12-17 15:33:29 +0100

    first step

--- src/components/RestaurantImage/RestaurantImage.components.tsx
+++ src/components/RestaurantImage/RestaurantImage.components.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import "./RestaurantImage.components.css"
 
-export default class RestaurantImage extends Component<{image: any}, {}> {
+export default class RestaurantImage extends Component<{image: any, name: any}, {}> {
 
     constructor(props: any) {
         super(props);
@@ -10,12 +10,20 @@ export default class RestaurantImage extends Component<{image: any}, {}> {
      componentDidMount() {
     }
 
+
     render() {
         const img = this.props.image;
+        const name = this.props.name;
         console.log(img)
         return (
-            <div className="image">
-                 {img && <img className="head-image" src={img[0].link}/>}         
+            <div>
+                <div className="image-outer-box">
+                    {img && <img className="head-image" src={img[0].link}/>}
+                    <div className="image-text-box">
+                        <p>{name}</p>
+                    </div>   
+                </div>
+
             </div>
         )
     }
--- src/pages/Reservation/Reservation.page.tsx
+++ src/pages/Reservation/Reservation.page.tsx
@@ -11,16 +11,16 @@ export default class Reservation extends Component<{location: any},{}> {
 
     render() {
         const img = this.props.location.state.data.images;
+        const name = this.props.location.state.data.name;
         const rest = this.props.location.state.data;
         console.log(img)
         return (
             <div>
                 <div className="image">
-                    <RestaurantImage image={img} />
+                    <RestaurantImage image={img} name={name}/>
                 </div>
                 <div className="restaurant-info">
                     <RestaurantInfos restaurant={rest}/>
-
                 </div>
                 <div className="reservation-form">
                     <ReservationInput restaurant={rest}/>
